0dbb56e0081f0a8d6bfd10bb4a5aa7715444f6cb
VBPFelipe <felipe.vbpinheiro@gmail.com>
2021-07-21 01:02:55 -0300

    main classes to test in test classes

--- src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -5,11 +5,15 @@ import java.math.BigDecimal;
 import br.com.alura.tdd.modelo.Desempenho;
 import br.com.alura.tdd.modelo.Funcionario;
 
+/*
+ * Classes que contenham regra de negócio, tendem a ser classes que devem 
+ * ter uma classe de teste. 
+ */
 public class ReajusteService {
 
-	public void concederReajuste(Funcionario funcionario,
-			Desempenho desempenho) {
-		BigDecimal reajuste = desempenho.percentualReajuste();
+	public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
+		BigDecimal percentual = desempenho.percentualReajuste(); 
+		BigDecimal reajuste = funcionario.getSalario().multiply(percentual);
 		funcionario.reajustarSalario(reajuste);
 	}
 
